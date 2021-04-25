65f11f6462a9964e6901077dda3e169519e89238
theiconik <kumarujjawal51@gmail.com>
2021-04-25 21:53:31 +0530

    Basic Homepage layout and navbar created

---
+++ components/Layout.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import styles from "../styles/Layout.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>विश्व</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className={styles.header}>
+        <Link href="/">
+          <div className={styles.div}>
+            <Image
+              src="/logo.png"
+              alt="logo"
+              width={30}
+              height={30}
+              alt="kuchnahi"
+            />
+            <h1>विश्व</h1>
+          </div>
+        </Link>
+
+        {/* <button className={styles.themeSwitcher} onClick={switchTheme}>
+          <Brightness6Rounded />
+        </button> */}
+      </header>
+
+      <main className={styles.main}>main</main>
+
+      <footer className={styles.footer}>Made with 💗</footer>
+    </div>
+  );
+};
+
+export default Layout;
